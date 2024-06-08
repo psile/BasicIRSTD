c654e1eb79417019d49dacb06f9a80b7115fecbe
psile <2193738978@qq.com>
2024-06-08 14:53:22 +0800

    7 push

--- model/ACM/model_ACM.py
+++ model/ACM/model_ACM.py
@@ -6,7 +6,7 @@ from  torchvision.models.resnet import BasicBlock
 from .fusion import AsymBiChaFuse
 import torch.nn.functional as F
 # from model.utils import init_weights, count_param
-
+import pdb
 class ASKCResUNet(nn.Module):
     def __init__(self, in_channels=1, layers=[3,3,3], channels=[8,16,32,64], fuse_mode='AsymBi', tiny=False, classes=1,
                  norm_layer=BatchNorm2d,groups=1, norm_kwargs=None, **kwargs):
@@ -72,6 +72,13 @@ class ASKCResUNet(nn.Module):
         self.fuse1 = self._fuse_layer(fuse_mode, channels=channels[1])
 
         self.head = _FCNHead(in_channels=channels[1], channels=classes, momentum=self.momentum)
+        self.Rep1=RepBlock(16,16)
+        self.Rep2=RepBlock(16,16)
+        self.Rep3=RepBlock(32,32)
+        self.Rep4=RepBlock(64,64)
+        self.Rep5=RepBlock(32,32)
+        self.Rep6=RepBlock(16,16)
+
 
 
     def _make_layer(self, block, out_channels, in_channels, blocks, stride):
@@ -105,18 +112,23 @@ class ASKCResUNet(nn.Module):
         _, _, hei, wid = x.shape
 
         x = self.stem(x)      # (4,16,120,120)
+        x=self.Rep1(x)
         c1 = self.layer1(x)   # (4,16,120,120)
+        c1=self.Rep2(c1)
         c2 = self.layer2(c1)  # (4,32, 60, 60)
+        c2=self.Rep3(c2)
         c3 = self.layer3(c2)  # (4,64, 30, 30)
-
+        c3=self.Rep4(c3)
         deconvc2 = self.deconv2(c3)        # (4,32, 60, 60)
         fusec2 = self.fuse2(deconvc2, c2)  # (4,32, 60, 60)
-        upc2 = self.uplayer2(fusec2)       # (4,32, 60, 60)
 
+        upc2 = self.uplayer2(fusec2)       # (4,32, 60, 60)
+        upc2=self.Rep5(upc2)
+        #pdb.set_trace()
         deconvc1 = self.deconv1(upc2)        # (4,16,120,120)
         fusec1 = self.fuse1(deconvc1, c1)    # (4,16,120,120)
         upc1 = self.uplayer1(fusec1)         # (4,16,120,120)
-
+        upc1=self.Rep6(upc1)
         pred = self.head(upc1)               # (4,1,120,120)
 
         if self.tiny:
@@ -124,7 +136,9 @@ class ASKCResUNet(nn.Module):
         else:
             # out = F.contrib.BilinearResize2D(pred, height=hei, width=wid)  # down 4
             out = F.interpolate(pred, scale_factor=4, mode='bilinear')  # down 4             # (4,1,480,480)
-
+        #out=out.sigmoid()
+        #out=torch.where(out > 0.5, torch.ones_like(out), torch.zeros_like(out))
+        #pdb.set_trace()
         return out.sigmoid()
 
     def evaluate(self, x):
@@ -151,9 +165,73 @@ class _FCNHead(nn.Module):
 def conv1x1(in_planes, out_planes, stride=1):
     """1x1 convolution"""
     return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+'''我加的'''
+class ChannelAttention(nn.Module):
 
+    def __init__(self, input_channels, internal_neurons):
+        super(ChannelAttention, self).__init__()
+        self.fc1 = nn.Conv2d(in_channels=input_channels, out_channels=internal_neurons, kernel_size=1, stride=1, bias=True)
+        self.fc2 = nn.Conv2d(in_channels=internal_neurons, out_channels=input_channels, kernel_size=1, stride=1, bias=True)
+        self.input_channels = input_channels
 
+    def forward(self, inputs):
+        x1 = F.adaptive_avg_pool2d(inputs, output_size=(1, 1))
+        # print('x:', x.shape)
+        x1 = self.fc1(x1)
+        x1 = F.relu(x1, inplace=True)
+        x1 = self.fc2(x1)
+        x1 = torch.sigmoid(x1)
+        x2 = F.adaptive_max_pool2d(inputs, output_size=(1, 1))
+        # print('x:', x.shape)
+        x2 = self.fc1(x2)
+        x2 = F.relu(x2, inplace=True)
+        x2 = self.fc2(x2)
+        x2 = torch.sigmoid(x2)
+        x = x1 + x2
+        x = x.view(-1, self.input_channels, 1, 1)
+        return x
+class RepBlock(nn.Module):
 
+    def __init__(self, in_channels, out_channels,
+                 channelAttention_reduce=4):
+        super().__init__()
+
+        self.C = in_channels
+        self.O = out_channels
+
+        assert in_channels == out_channels
+        self.ca = ChannelAttention(input_channels=in_channels, internal_neurons=in_channels // channelAttention_reduce)
+        self.dconv5_5 = nn.Conv2d(in_channels,in_channels,kernel_size=5,padding=2,groups=in_channels)
+        self.dconv1_7 = nn.Conv2d(in_channels,in_channels,kernel_size=(1,7),padding=(0,3),groups=in_channels)
+        self.dconv7_1 = nn.Conv2d(in_channels,in_channels,kernel_size=(7,1),padding=(3,0),groups=in_channels)
+        self.dconv1_11 = nn.Conv2d(in_channels,in_channels,kernel_size=(1,11),padding=(0,5),groups=in_channels)
+        self.dconv11_1 = nn.Conv2d(in_channels,in_channels,kernel_size=(11,1),padding=(5,0),groups=in_channels)
+        self.dconv1_21 = nn.Conv2d(in_channels,in_channels,kernel_size=(1,21),padding=(0,10),groups=in_channels)
+        self.dconv21_1 = nn.Conv2d(in_channels,in_channels,kernel_size=(21,1),padding=(10,0),groups=in_channels)
+        self.conv = nn.Conv2d(in_channels,in_channels,kernel_size=(1,1),padding=0)
+        self.act = nn.GELU()
+
+    def forward(self, inputs):
+        #   Global Perceptron
+        inputs = self.conv(inputs)
+        inputs = self.act(inputs)
+        
+        channel_att_vec = self.ca(inputs)
+        inputs = channel_att_vec * inputs #(1,64,32,32)
+        #pdb.set_trace()
+        x_init = self.dconv5_5(inputs) #(1,64,32,32)
+        x_1 = self.dconv1_7(x_init)
+        x_1 = self.dconv7_1(x_1)
+        x_2 = self.dconv1_11(x_init)
+        x_2 = self.dconv11_1(x_2)
+        x_3 = self.dconv1_21(x_init)
+        x_3 = self.dconv21_1(x_3)
+        x = x_1 + x_2 + x_3 + x_init
+        spatial_att = self.conv(x) #(1,64,32,32)
+        out = spatial_att * inputs
+        out = self.conv(out) #(1,64,32,32)
+        return out
+'''end'''
 #########################################################
 ###2.测试ASKCResUNet
 if __name__ == '__main__':
