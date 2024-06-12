73122ab41d3f5e8758e976f8473db02b07d871c7
psile <2193738978@qq.com>
2024-06-13 00:27:17 +0800

    16 push

--- inference.py
+++ inference.py
@@ -62,10 +62,10 @@ def test():
 
     with torch.no_grad():
         for idx_iter, (img, size, img_dir) in tqdm(enumerate(test_loader)):
-            img, h,w = downsample_if_needed(img)
+            # img, h,w = downsample_if_needed(img)
             img = Variable(img).cuda()
             pred = net.forward(img)
-            pred=F.interpolate(pred, size=(h, w), mode='bilinear', align_corners=False)
+            # pred=F.interpolate(pred, size=(h, w), mode='bilinear', align_corners=False)
             pred = pred[:,:,:size[0],:size[1]]        
             ### save img
             if opt.save_img == True:
