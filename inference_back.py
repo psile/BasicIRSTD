9f6126bd2071807bbbf213a6716107fa06325e2f
psile <2193738978@qq.com>
2024-06-13 12:58:38 +0800

    26 push

--- inference_back.py
+++ inference_back.py
@@ -2,32 +2,33 @@ import argparse
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
 from net import Net
-from model.dataset import *
+from dataset import *
 import matplotlib.pyplot as plt
 from metrics import *
 import os
 import time
 from tqdm import tqdm
+import torch.nn.functional as F
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
 parser = argparse.ArgumentParser(description="PyTorch BasicIRSTD Inference without mask")
-parser.add_argument("--model_names", default=['ACM', 'ALCNet','DNANet', 'ISNet', 'RDIAN', 'ISTDU-Net'], nargs='+',  
+parser.add_argument("--model_names", default=['ACM'], nargs='+',  
                     help="model_name: 'ACM', 'ALCNet', 'DNANet', 'ISNet', 'UIUNet', 'RDIAN', 'ISTDU-Net', 'U-Net', 'RISTDnet'")
 parser.add_argument("--pth_dirs", default=None, nargs='+',  help="checkpoint dir, default=None or ['NUDT-SIRST/ACM_400.pth.tar','NUAA-SIRST/ACM_400.pth.tar']")
 parser.add_argument("--dataset_dir", default='./datasets', type=str, help="train_dataset_dir")
-parser.add_argument("--dataset_names", default=['NUAA-SIRST', 'NUDT-SIRST', 'IRSTD-1K'], nargs='+', 
+parser.add_argument("--dataset_names", default=['PRCV2024'], nargs='+', 
                     help="dataset_name: 'NUAA-SIRST', 'NUDT-SIRST', 'IRSTD-1K', 'SIRST3', 'NUDT-SIRST-Sea'")
 parser.add_argument("--img_norm_cfg", default=None, type=dict,
                     help="specific a img_norm_cfg, default=None (using img_norm_cfg values of each dataset)")
-parser.add_argument("--img_norm_cfg_mean", default=None, type=float,
+parser.add_argument("--img_norm_cfg_mean", default=107.848, type=float,
                     help="specific a mean value img_norm_cfg, default=None (using img_norm_cfg values of each dataset)")
-parser.add_argument("--img_norm_cfg_std", default=None, type=float,
+parser.add_argument("--img_norm_cfg_std", default=30.905, type=float,
                     help="specific a std value img_norm_cfg, default=None (using img_norm_cfg values of each dataset)")
 
 parser.add_argument("--save_img", default=True, type=bool, help="save image of or not")
 parser.add_argument("--save_img_dir", type=str, default='./results/', help="path of saved image")
 parser.add_argument("--save_log", type=str, default='./log/', help="path of saved .pth")
 parser.add_argument("--threshold", type=float, default=0.5)
-
+import pdb
 global opt
 opt = parser.parse_args()
 ## Set img_norm_cfg
@@ -35,10 +36,24 @@ if opt.img_norm_cfg_mean != None and opt.img_norm_cfg_std != None:
   opt.img_norm_cfg = dict()
   opt.img_norm_cfg['mean'] = opt.img_norm_cfg_mean
   opt.img_norm_cfg['std'] = opt.img_norm_cfg_std
-  
+def downsample_if_needed(img, size_limit=512):
+    """如果图像尺寸超过限制，进行下采样"""
+    _,_,h, w = img.shape
+    if max(h, w) > size_limit:
+        scale_factor = size_limit / max(h, w)
+        new_h = int(h * scale_factor)
+        new_w = int(w * scale_factor)
+        # 调整尺寸为8的倍数
+        new_h = ((new_h + 15) // 16) * 16  # 向上取最接近的8的倍数
+        new_w = ((new_w + 15) // 16) * 16
+        img=F.interpolate(img, size=(new_h, new_w), mode='bilinear', align_corners=False)
+        #img = img.resize((new_w, new_h), resample=Image.BILINEAR)
+        return img, h,w #,True
+    else:
+        return img, h,w  #,False
 def test(): 
     test_set = InferenceSetLoader(opt.dataset_dir, opt.train_dataset_name, opt.test_dataset_name, opt.img_norm_cfg)
-    test_loader = DataLoader(dataset=test_set, num_workers=1, batch_size=1, shuffle=False)
+    test_loader = DataLoader(dataset=test_set, num_workers=1, batch_size=1, shuffle=False)#,pin_memory=True
     
     net = Net(model_name=opt.model_name, mode='test').cuda()
     try:
@@ -48,63 +63,23 @@ def test():
         net.load_state_dict(torch.load(opt.pth_dir, map_location=device)['state_dict'])
     net.eval()
 
-    # with torch.no_grad():
-    #     for idx_iter, (img, size, img_dir) in tqdm(enumerate(test_loader)):
-    #         img = Variable(img).cuda()
-    #         pred = net.forward(img)
-    #         pred = pred[:,:,:size[0],:size[1]]
-
-    max_block_size = (512, 512)
     with torch.no_grad():
         for idx_iter, (img, size, img_dir) in tqdm(enumerate(test_loader)):
+            #pdb.set_trace()
+            img, h,w = downsample_if_needed(img)
+            
             img = Variable(img).cuda()
-            _, _, height, width = img.size()
-
-            # 计算需要填充的尺寸
-            pad_height = (max_block_size[0] - height % max_block_size[0]) % max_block_size[0] # 512 - 832 % 512 = 192
-            pad_width = (max_block_size[1] - width % max_block_size[1]) % max_block_size[1] # 512 - 1088 % 512 = 448
-          
-            # 对图像进行填充
-            img = F.pad(img, (0, 0, pad_width, pad_height), padding_mode='constant', fill=0)
-            _, _, padded_height, padded_width = img.size()
-
-            num_blocks_height = (padded_height + max_block_size[0] - 1) // max_block_size[0]
-            num_blocks_width = (padded_width + max_block_size[1] - 1) // max_block_size[1]
-
-            # 动态分块推理
-            output = torch.zeros_like(img)
-            for i in range(num_blocks_height):
-                for j in range(num_blocks_width):
-                    block_y = i * max_block_size[0]
-                    block_x = j * max_block_size[1]
-                    block_height = min(max_block_size[0], padded_height - block_y)
-                    block_width = min(max_block_size[1], padded_width - block_x)
-
-                    # 确保块的尺寸大于0
-                    if block_height <= 0 or block_width <= 0:
-                        print(f'Skipping block at (i={i}, j={j}) due to zero or negative size: height={block_height}, width={block_width}')
-                        continue
-
-                    block = img[:, :, block_y:block_y + block_height, block_x:block_x + block_width]
-                    
-
-                    try:
-                        pred_block = net.forward(block)
-                    except RuntimeError as e:
-                        print(f'Error processing block at (i={i}, j={j}): {str(e)}')
-                        continue
-
-                    output[:, :, block_y:block_y + block_height, block_x:block_x + block_width] = pred_block
-
-            # 去除填充部分
-            output = output[:,:,:size[0],:size[1]]
-            pred = output     
+            pred = net.forward(img)
+            pred=F.interpolate(pred, size=(h, w), mode='bilinear', align_corners=False)
+            pred = pred[:,:,:size[0],:size[1]]        
             ### save img
             if opt.save_img == True:
                 img_save = transforms.ToPILImage()(((pred[0,0,:,:]>opt.threshold).float()).cpu())
                 if not os.path.exists(opt.save_img_dir + opt.test_dataset_name + '/' + opt.model_name):
                     os.makedirs(opt.save_img_dir + opt.test_dataset_name + '/' + opt.model_name)
-                img_save.save(opt.save_img_dir + opt.test_dataset_name + '/' + opt.model_name + '/' + img_dir[0] + '.png')  
+                img_save.save(opt.save_img_dir + opt.test_dataset_name + '/' + opt.model_name + '/' + img_dir[0] + '.png') 
+            del img, pred, img_save
+            torch.cuda.empty_cache()
     
     print('Inference Done!')
    
