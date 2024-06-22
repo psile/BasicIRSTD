42f2b94dc59db92114c22b09e7b1d1ed31f415f5
psile <2193738978@qq.com>
2024-06-22 18:07:07 +0800

    68 push

--- test_block.py
+++ test_block.py
@@ -65,9 +65,9 @@ def test():
             pad_width = (max_block_size[1] - width % max_block_size[1]) % max_block_size[1] # 512 - 1088 % 512 = 448
           
             # 对图像进行填充
-            img=F.pad(img, (0, 0, pad_width, pad_height), fill=0, padding_mode='constant')
+            #img=F.pad(img, (0, 0, pad_width, pad_height), fill=0, padding_mode='constant')
             # img = F.pad(img, (0, 0, pad_width, pad_height), padding_mode='constant', constant_values=0)#padding_mode
-            #img=F.pad(img, (0, pad_width,0, pad_height),mode='constant',value=0)
+            img=F.pad(img, (0, pad_width,0, pad_height),mode='constant',value=0)
             _, _, padded_height, padded_width = img.size()
 
             num_blocks_height = (padded_height + max_block_size[0] - 1) // max_block_size[0]
